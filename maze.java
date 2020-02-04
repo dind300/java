db977b4191bf7f4b8040a7a2dc7e8daa9a29d73f
dind300 <60182756+dind300@users.noreply.github.com>
2020-02-03 18:00:04 -0800

    Update maze.java

--- maze.java
+++ maze.java
@@ -1,52 +1,55 @@
 public class maze {
 	public static void main(String[] args) {
-		boolean[][] matrix = { { true, true,false,true,true,true}, {true, false,false,true, false,true },
-				{true,true,true,true,false,true }, {false,false, false,false,true,true },{false,false,false,false,false,true},{true,true,true,true,true,true} };
+		boolean[][] matrix = { { true, true, false, true, true, true }, { true, false, false, true, false, true },
+				{ true, true, true, true, false, true }, { false, false, false, false, true, true },
+				{ false, false, false, false, false, true }, { true, true, true, true, true, true } };
 
-		System.out.println(findTheExit(matrix, 0, 0,"matrix"));
+		System.out.println(findTheExit(matrix, 0, 0, "matrix"));
 	}
 
-	public static boolean findTheExit(boolean[][] matrix, int row, int col,String path) {
-		if (row == matrix.length - 1 && col == matrix[0].length - 1) {
-			System.out.print(row + "," + col+" <- ");
+	public static boolean findTheExit(boolean[][] matrix, int row, int col, String path) {
+		if (row == matrix.length - 1 && col == matrix[0].length - 1) {//base condition
+			System.out.print(row + "," + col + " <- ");
 			return true;
 		}
-		if (!matrix[row][col]) {
-				return false;
+		if (!matrix[row][col]) {//base condition
+			return false;
 		}
-		
-		if (col < matrix[0].length - 1) {
-			if (findTheExit(matrix, row, col + 1,"left")) {
-					System.out.print(row + "," + col+" <- ");
-					return true;
-				}
-			}
-		if (row < matrix.length) {
-			if (findTheExit(matrix, row + 1, col,"up")) {
-					System.out.print(row + "," + col+" <- ");
-					return true;
-				}
-			}
-		
-		 if (col > 0) {
-			if(path!="left") {
-				matrix[row][col]=false;
-			if (findTheExit(matrix, row, col - 1,"right") ) {
-				System.out.print(row + "," + col+ " <- ");
-				return true;
-			}
-			}
-		}
-		if (row > 0) {
-			if(path!="up") {
-			matrix[row][col]=false;
-			if (findTheExit(matrix, row - 1, col,"down")) {
-					System.out.print(row + "," + col+ " <- ");
+	
+		if (col < matrix[0].length - 1) {	//check out of bounds
+			if (path != "right") {//check if not backtracking
+				if (findTheExit(matrix, row, col + 1, "left")) {//check right step
+					System.out.print(row + "," + col + " <- ");
 					return true;
 				}
 			}
 		}
-		return false;
+		if (row < matrix.length) {	//check out of bounds
+			if (path != "down") {//check if not backtracking
+				if (findTheExit(matrix, row + 1, col, "up")) {//check down step
+					System.out.print(row + "," + col + " <- ");
+					return true;
+				}
+			}
+		}
+
+		if (col > 0) {	//check out of bounds
+			if (path != "left") {//check if not backtracking
+				if (findTheExit(matrix, row, col - 1, "right")) {//check left step
+					System.out.print(row + "," + col + " <- ");
+					return true;
+				}
+			}
+		}
+		if (row > 0) {	//check out of bounds
+			if (path != "up") {//check if not backtracking
+				if (findTheExit(matrix, row - 1, col, "down")) {//check up step
+					System.out.print(row + "," + col + " <- ");
+					return true;
+				}
+			}
+		}
+		return false;//if all not work return false.
 	}
 
 }
