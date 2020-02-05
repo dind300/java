27b7650daad45f78ac2d076a8311586de36637f1
dind300 <60182756+dind300@users.noreply.github.com>
2020-02-05 04:24:54 -0800

    Update maze.java

--- maze.java
+++ maze.java
@@ -1,12 +1,12 @@
 public class maze {
 	public static void main(String[] args) {
-		boolean[][] matrix = { { true, true, false, true, true, true }, 
-				       { true, false, false, true, false, true },
-				       { true, true, true, true, false, true },
-				       { false, false, false, false, true, true },
-				       { false, false, false, false, false, true }, 
-				       { true, true, true, true, true, true } };
- 
+		boolean[][] matrix = {  { true, true, true, true, true, true }, 
+					{ true, false, false, true, false, true },
+					{ true, true, true, true, false, true }, 
+					{ true, false, false, true, false, true },
+					{ true, false, false, true, false, true }, 
+					{ true, true, true, false, true, true } };
+
 		System.out.println(isMazeSolved(matrix));
 	}
 
@@ -21,6 +21,23 @@ public class maze {
 		}
 		if (!matrix[row][col]) {// base condition
 			return false;
+		}
+		matrix[row][col]=false;
+		if (col > 0) { // check out of bounds
+			if (path != "left") {// check if not backtracking
+				if (findTheExit(matrix, row, col - 1, "right")) {// check left step
+					System.out.print(row + "," + col + " -> ");
+					return true;
+				}
+			}
+		}
+		if (row > 0) { // check out of bounds
+			if (path != "up") {// check if not backtracking
+				if (findTheExit(matrix, row - 1, col, "down")) {// check up step
+					System.out.print(row + "," + col + " -> ");
+					return true;
+				}
+			}
 		}
 
 		if (col < matrix[0].length - 1) { // check out of bounds
@@ -40,22 +57,6 @@ public class maze {
 			}
 		}
 
-		if (col > 0) { // check out of bounds
-			if (path != "left") {// check if not backtracking
-				if (findTheExit(matrix, row, col - 1, "right")) {// check left step
-					System.out.print(row + "," + col + " -> ");
-					return true;
-				}
-			}
-		}
-		if (row > 0) { // check out of bounds
-			if (path != "up") {// check if not backtracking
-				if (findTheExit(matrix, row - 1, col, "down")) {// check up step
-					System.out.print(row + "," + col + " -> ");
-					return true;
-				}
-			}
-		}
 		return false;// if all not work return false.
 	}
 
